ca9243bc8b6f55d5b33e425c50f77c954fd51bc7
[SW5] 홍승진 <cmikal47@gmail.com>
2023-07-04 17:04:26 +0900

    fix: productSchema의 category 누락 수정

--- src/models/product.js
+++ src/models/product.js
@@ -6,6 +6,7 @@ const productSchema = new Schema({
   title: { type: String, required: true },
   price: { type: Number, required: true },
   description: { type: String, required: true },
+  category: { type: String, required: true },
   imageUrl: { type: String, required: true },
 });
 
