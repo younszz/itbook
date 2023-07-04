913b74d277dd2298a646347c3ceec7bcf0c794f3
[SW5] 홍승진 <cmikal47@gmail.com>
2023-07-04 14:25:31 +0900

    feat: MongoDB 연결, express.static public폴더  설정

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
 import express from 'express';
 import serveStatic from './src/routes/serve-static.js';
-
+import bodyParser  from 'body-parser';
+import mongoose from 'mongoose';
 const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use('/', serveStatic('home'));
 
-app.listen(3000, () => {
-  console.log(`Server is running on http://localhost:${3000}`);
-});
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+mongoose
+  .connect(
+    'mongodb+srv://Gwanggaeto:Gwang1234@cluster0.o8ndafw.mongodb.net/?retryWrites=true&w=majority'
+  )
+  .then(() => {
+    app.listen(3000);
+  })
+  .catch((err) => console.log(err));
