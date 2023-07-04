2ea6e4b903e1067dde5a090683eea89e904c0799
yoonji12 <dbswl342@gmail.com>
2023-07-04 17:50:13 +0900

    templete: admin 페이지(상품목록, 상품등록/수정, 주문목록) 추가

--- src/views/home/home.js
+++ src/views/home/home.js
@@ -55,3 +55,29 @@ var swiper = new Swiper(".list-swiper", {
   },
   loop: true,
 });
+
+
+// 데이터 바인딩 함수
+async function fetchProducts() {
+  try {
+    const bookList = document.getElementById('bookList');
+    const response = await fetch('/products');
+    const data = await response.json();
+
+    const books = data.map(book => 
+    <div class="list-box">
+      <a><img src=${book.imageUrl} alt="" /></a>
+      <div class="info">
+        <p class="cate new">${book.category}</p>
+        <p class="name">${book.title}</p>
+        <p class="price">${book.description}</p>
+      </div>
+    </div>
+    ).join('')
+    bookList.innerHTML = books;
+  } catch (error) {
+    console.error('Error:', error);
+  }
+}
+
+fetchProducts();
