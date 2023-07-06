1b2b9244ee6a932b00a33d8cd963d027b1d54d62
yoonji12 <dbswl342@gmail.com>
2023-07-07 02:39:18 +0900

    feat: 관리자페이지 상품 추가 기능 구현

--- src/views/admin-products/admin-products.js
+++ src/views/admin-products/admin-products.js
@@ -1,13 +1,38 @@
+//데이터 바인딩 함수
 async function fetchProducts() {
   try {
-    const response = await fetch('/products');
+    const bookList = document.getElementById('adminTbl');
+    const response = await fetch('/api/products');
+    console.log(response);
     const data = await response.json();
-    console.log(data)
+    const books = data
+      .map(
+        (book) =>
+          `<tr>
+            <td><input type="checkbox" /></td>
+            <td class="p-img">
+              <img src=${book.imageUrl} alt="" />
+            </td>
+            <td>${book.title}</td>
+            <td>${book.price}원</td>
+            <td>${book.category}</td>
+            <td>
+              <button class="admin-change"><a href="/admin/products/edit/:pid">수정</a></button>
+              <button class="admin-remove-in">삭제</button>
+            </td>
+          </tr>`
+      )
+      .join('');
     
+    bookList.innerHTML = books;
 
+    const listCount = document.getElementById('listCount');
+    const count = data.length;
+    listCount.innerHTML = count;
   } catch (error) {
     console.error('Error:', error);
   }
 }
 
 fetchProducts();
+
