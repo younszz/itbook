0aa7329b631275bd026deb10e6961b3779b9b72c
[SW5] 홍승진 <cmikal47@gmail.com>
2023-07-06 18:41:08 +0900

    style: api endpoint 누락

--- src/views/admin-products/admin-products.js
+++ src/views/admin-products/admin-products.js
@@ -1,6 +1,6 @@
 async function fetchProducts() {
   try {
-    const response = await fetch('/products');
+    const response = await fetch('/api/products');
     const data = await response.json();
     console.log(data)
     
