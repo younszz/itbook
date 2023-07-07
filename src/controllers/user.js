0a1f5885e2c5356be7f0d17b67ce28ecd022792d
[SW5] 홍승진 <cmikal47@gmail.com>
2023-07-08 00:00:09 +0900

    feat : user api

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -37,3 +37,38 @@ exports.postLogin = async (req, res) => {
     res.status(500).send("로그인 중 오류가 발생하였습니다.");
   }
 };
+
+exports.getUser = async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id);
+    if (!user) throw Error("사용자가 존재하지 않습니다.");
+    res.json(user);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: " 문제가 발생했습니다."});
+  }
+};
+
+//사용자 정보 수정
+exports.updateUser = async (req, res) => {
+  try {
+    const user = await User.findByIdAndUpdate(req.user.id, req.body, { new: true});
+    if (!user) throw Error("사용자가 존재하지 않습니다.");
+    res.json(user);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: " 문제가 발생했습니다. "})
+  }
+};
+
+//사용자 정보 삭제
+exports.deleteUser = async (req, res) =>{
+  try {
+    const user = await User.findByIdandDelete(req.user.id);
+    if (!user) throw Error("사용자가 존재하지 않습니다.");
+    res.json({ message: "사용자 삭제가 완료되었습니다."});
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: " 문제가 발생했습니다."})
+  }
+};
--- src/routes/user.js
+++ src/routes/user.js
@@ -1,9 +1,16 @@
 const express = require("express");
 const userController = require("../controllers/user");
-
+const passport = require("passport");
 const router = express.Router();
 
 router.post("/api/join", userController.postJoin);
 router.post("/api/login", userController.postLogin);
+
+// 사용자 정보 조회
+router.get("/api/user", passport.authenticate('jwt', { session: false}), userController.getUser);
+// 사용자 정보 수정
+router.post("/api/user", passport.authenticate('jwt', { session: false }), userController.updateUser);
+// 사용자 정보 삭제
+router.delete("/api/user/:uid", passport.authenticate('jwt', { session: false}), userController.deleteUser);
 
 module.exports = router;
