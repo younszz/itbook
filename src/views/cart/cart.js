877be1eba45780f676d59329de690d0682ab2197
[SW5] 김진규 <rlawlsrb16736@gmail.com>
2023-07-07 16:58:50 +0900

    feat: 상품 데이터 받아와서 장바구니페이지에 상품목록 렌더링

--- src/views/cart/cart.js
+++ src/views/cart/cart.js
@@ -5,16 +5,52 @@ function getLocalBooks(){
   return books;
 }
 
-async function getServerBook(){
-  const books = getLocalBooks();
-  console.log(books);
-  const bookId = books[0].id;
-  const response = await fetch(`/api/product/${bookId}`);
-  const book = await response.json();
-  console.log(book);
+async function getServerBooks(){
+  const localBooks = getLocalBooks();
+  const booksLength = localBooks.length;
+  const books = [];
+  for(let i = 0; i < booksLength; i++){
+    const bookId = localBooks[i].id;
+    const quantity = localBooks[i].quantity;
+    const response = await fetch(`/api/product/${bookId}`);
+    const book = await response.json();
+    books.push({...book, quantity});
+  }
+  return books;
 }
 
-getServerBook();
+async function renderBooks(){
+  const books = await getServerBooks();
+  const ul = document.querySelector('.cart-list');
+  books.forEach((book)=>{
+    const li = document.createElement('li');
+    li.setAttribute('class','cart-item');
+    li.innerHTML = itemTemplate(book);
+    ul.append(li);
+  });
+}
+
+renderBooks();
+
+function itemTemplate(book){
+  return `
+  <input type="checkbox" class="selectCheck" checked>
+  <a href="#" class="item-info">
+    <img src="${book.imageUrl}" alt="도서 사진">
+    <div class="info-text">
+      <p class="info-title">${book.title}</p>
+      <p class="description">${book.author}</p>
+    </div>
+  </a>
+  <div class="item-count">
+    <button class="btn-minus"><i class="fa-solid fa-minus"></i></button>
+      <input type="number" id="item-number" value=${book.quantity}>
+      <button class="btn-plus"><i class="fa-solid fa-plus"></i></button>
+  </div>
+  <p class="item-price">${book.price * book.quantity}원</p>
+  <button class="btn-delete"><i class="fa-solid fa-xmark"></i></button>
+  `;
+}
 
 // window.addEventListener('load',()=>{
 //   const ul = document.querySelector('.cart-list');
@@ -72,22 +108,3 @@ getServerBook();
 //   }
 // }
 
-// function itemTemplate(book){
-//   return `
-//   <input type="checkbox" class="selectCheck" checked>
-//   <a href="#" class="item-info">
-//     <img src="${book.imageUrl}" alt="도서 사진">
-//     <div class="info-text">
-//       <p class="info-title">${book.title}</p>
-//       <p class="description">${book.author}</p>
-//     </div>
-//   </a>
-//   <div class="item-count">
-//     <button class="btn-minus"><i class="fa-solid fa-minus"></i></button>
-//       <input type="number" id="item-number" value=${book.count}>
-//       <button class="btn-plus"><i class="fa-solid fa-plus"></i></button>
-//   </div>
-//   <p class="item-price">${book.price * book.count}원</p>
-//   <button class="btn-delete"><i class="fa-solid fa-xmark"></i></button>
-//   `;
-// }
