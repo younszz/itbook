21d6f1984ba74fa3d27bf40d1d1578b29cfe3bbb
[SW5] 홍승진 <cmikal47@gmail.com>
2023-07-07 16:56:41 +0900

    feat: admin-category

--- src/routes/shop.js
+++ src/routes/shop.js
@@ -6,4 +6,8 @@ router.get('/api/products', shopController.getProducts);
 
 router.get('/api/product/:pid', shopController.getproductDetail);
 
+router.get('/api/category', shopController.getCategories);
+
+router.post('/api/category', shopController.updateCategory);
+
 module.exports = router; 
