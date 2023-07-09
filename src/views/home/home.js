b1feaf7200852048ab0a0c889d83de2313bfd07d
yoonji12 <dbswl342@gmail.com>
2023-07-09 12:10:39 +0900

    feat : 카테고리별 색상 다르게 적용

--- src/views/home/home.js
+++ src/views/home/home.js
@@ -26,26 +26,50 @@ async function fetchProducts() {
   try {
     const bookList = document.getElementById('bookList');
     const response = await fetch('/api/products');
-    console.log(response)
     const data = await response.json();
     const books = data
       .map(
         (book) =>
           `<div class="list-box">
-      <a href="/product/${book._id}">
-      <div class="img-wrap">
-        <img src=${book.imageUrl} alt="" />
-      </div>
-      <div class="info">
-        <p class="cate new">${book.category}</p>
-        <p class="name">${book.title}</p>
-        <p class="price">${book.price}원</p>
-      </div>
-      </a>
-    </div>`
+            <a href="/product/${book._id}">
+              <div class="img-wrap">
+                <img src=${book.imageUrl} alt="" />
+              </div>
+              <div class="info">
+                <p class="cate">${book.category}</p>
+                <p class="name">${book.title}</p>
+                <p class="price">${book.price}원</p>
+              </div>
+            </a>
+          </div>`
       )
       .join('');
     bookList.innerHTML = books;
+
+    // 카테고리 별 색상 적용
+    categoryColor();
+  } catch (error) {
+    console.error('Error:', error);
+  }
+}
+
+async function categoryColor() {
+  try {
+    const cateList = document.getElementsByClassName("cate");
+    Array.from(cateList).forEach((cate) => {
+      const category = cate.textContent;
+      if (category === "웹") {
+        cate.classList.add("green");
+      } else if (category === "Ai") {
+        cate.classList.add("orange");
+      } else if (category === "알고리즘") {
+        cate.classList.add("blue");
+      } else if (category === "모바일") {
+        cate.classList.add("pink");
+      } else{
+        cate.classList.add("purple");
+      } 
+    });
   } catch (error) {
     console.error('Error:', error);
   }
