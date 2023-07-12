08ff378151cd8d6ff9384750f356946361ae1412
[SW5] 김경연 <argand0304@gmail.com>
2023-07-12 16:00:22 +0900

    fix:jwt_중복인식_해소

--- src/controllers/order.js
+++ src/controllers/order.js
@@ -2,24 +2,23 @@ import Order from '../models/Order';
 
 //주문 생성
 export const postOrder = async (req, res) => {
-    const { userId, products, totalAmount, receiverInfo } = req.body;
-    const order = new Order({
-      user: {
-        userId: userId,
-        name: req.user.name,
-      },
-      products: products,
-      totalAmount: totalAmount,
-      receiverInfo: receiverInfo
-    });
-    try {
-      await order.save();
-      res.status(201).json(order);
-    } catch (err) {
-      console.error(err);
-      res.status(500).json({ message: '주문 생성에 실패했습니다.' });
-    }
-  };
+  const { userId, products, totalAmount, deliveryAddress, recipientName, recipientContact } = req.body;
+  const order = new Order({
+    userId,
+    products,
+    totalAmount,
+    deliveryAddress,
+    recipientName,
+    recipientContact,
+  });
+  try {
+    await order.save();
+    res.status(201).json(order);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: '서버에 문제가 발생했습니다' });
+  }
+};
 
 
 
@@ -39,7 +38,7 @@ export const getAllOrders = async (req, res) => {
 // 주문 조회 - 사용자
 export const getMyOrders = async (req, res) => {
   try {
-    const orders = await Order.find({ "user.userId": req.user.id });
+    const orders = await Order.find({ 'userId': req.user.id });
     res.json(orders);
   } catch (err) {
     console.error(err);
@@ -55,12 +54,16 @@ export const updateOrderStatus = async (req, res) => {
 
   try {
     const order = await Order.findById(orderId);
+    if(!order) {
+      return res.status(404).json({message: '주문을 찾을 수 없습니다.'})
+    }
     order.deliveryStatus = deliveryStatus;
     const result = await order.save();
     console.log('배송 상태 수정');
     res.status(201).json(result);
   } catch (err) {
     console.log(err);
+    res.status(500).json({ message: '서버에 문제가 발생했습니다.' })
   }
 };
 
@@ -72,8 +75,11 @@ export const updateOrder = async (req, res) => {
 
   try {
     const order = await Order.findById(orderId);
+    if (!order) {
+      return res.status(404).json({ message: '주문을 찾을 수 없습니다.'});
+    }
     if (order.deliveryStatus !== '상품 준비중') {
-      throw new Error('배송이 시작되면 주문 정보를 수정할 수 없습니다.');
+      return res.status(400).json({ message: '배송이 시작되면 주문 정보를 수정할 수 없습니다.' });
     }
     order.deliveryAddress = deliveryAddress;
     order.recipientName = recipientName;
@@ -83,16 +89,20 @@ export const updateOrder = async (req, res) => {
     res.status(201).json(result);
   } catch (err) {
     console.log(err);
+    res.status(500).json({ message: '서버에 문제가 발생했습니다.'});
   }
 };
 
 
-// 주문 취소
+// 주문 취소 (사용자 - 배송전)
 export const cancelOrder = async (req, res) => {
   const orderId = req.params.oid;
 
   try {
     const order = await Order.findById(orderId);
+    if (!order) {
+      return res.status(404).json({ message: '주문을 찾을 수 없습니다.' });
+    }
     if (order.deliveryStatus !== '상품 준비중') {
       throw new Error('배송이 시작되면 주문을 취소할 수 없습니다.');
     }
@@ -101,6 +111,7 @@ export const cancelOrder = async (req, res) => {
     res.status(201).json({ message: '주문이 취소되었습니다.' });
   } catch (err) {
     console.log(err);
+    res.status(500).json({ message: '서버에 문제가 발생했습니다.' });
   }
 };
 
@@ -112,8 +123,9 @@ export const deleteOrder = async (req, res) => {
   try {
     await Order.findByIdAndRemove(orderId);
     console.log('주문 삭제');
-    res.status(201).json({ message: '주문이 삭제되었습니다.' });
+    res.status(200).json({ message: '주문이 삭제되었습니다.' });
   } catch (err) {
     console.log(err);
+    res.status(500).json({ message: '서버에 문제가 발생했습니다.' });
   }
 };
