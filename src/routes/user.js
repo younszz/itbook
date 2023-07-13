486db9756aeaef174eefad3598fb46ed22741b59
[SW5] 홍승진 <cmikal47@gmail.com>
2023-07-13 15:47:15 +0900

    fix models/User -> user

---
+++ src/routes/user.js
@@ -0,0 +1,34 @@
+import express from 'express';
+import {
+  getUser,
+  updateUser,
+  deleteUser,
+  addToCart,
+  getCart,
+  clearCart,
+  removeItem,
+  mergeCarts,
+  adjustQuantity
+} from '../controllers/user';
+
+const router = express.Router();
+
+router.get('/api/user', getUser);
+
+router.post('/api/user/', updateUser);
+
+router.delete('/api/user/:id/', deleteUser);
+
+router.get('/api/user/cart', getCart);
+
+router.put('/api/user/cart/:id/:direction', adjustQuantity);
+
+router.post('/api/user/cart/merge', mergeCarts);
+
+router.delete('/api/user/cart/:id', removeItem);
+
+router.post('/api/user/cart', addToCart);
+
+router.delete('/api/cart/all/:uid', clearCart);
+
+export default router;
