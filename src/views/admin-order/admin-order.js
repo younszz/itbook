54805292a375b40d77e8ae520244841228dc40ec
[SW5] 김진규 <rlawlsrb16736@gmail.com>
2023-07-13 12:07:12 +0900

    feat: 주문리스트 가져오는 기능

--- src/views/admin-order/admin-order.js
+++ src/views/admin-order/admin-order.js
@@ -1,48 +1,15 @@
-async function fetchProduct() {
-  const url = window.location.pathname;
-  const parts = url.split('/').filter(Boolean);
-  const id = parts.pop();
-}
-
-//데이터 바인딩 함수
-async function fetchProducts() {
-  try {
-    const bookList = document.getElementById('adminTbl');
-    const response = await fetch('/api/products');
-    console.log(response);
+const getOrderList = async () => {
+  try{
+    const response = await fetch('/api/orders');
     const data = await response.json();
-    const books = data
-      .map(
-        (book) =>
-        `<tr>
-        <td><input type="checkbox"></td>
-        <td>${book.title}</td>
-        <td>개</td>
-        <td>${book.price}원</td>
-        <td>원</td>
-        <td>
-          <select name="" id="">
-            <option value="">주문</option>
-            <option value="">배송</option>
-          </select>
-        </td>
-      </tr>`
-      )
-      .join('');
-    
-    bookList.innerHTML = books;
 
-    const listCount = document.getElementById('listCount');
-    const count = data.length;
-    listCount.innerHTML = count;
-  } catch (error) {
-    console.error('Error:', error);
+    return data;
+  }catch(e){
+    console.error(e);
   }
 }
+window.addEventListener("load", async ()=>{
 
-fetchProducts();
-
-//
-
-let data = $('#orderList').find("td:eq(1)").text();
-console.log(data);
+  const orderList = await getOrderList();
+  console.log(orderList);
+})
