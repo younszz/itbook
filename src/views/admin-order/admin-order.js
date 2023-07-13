6cbc06e83447be218c9ee17d28320d844a08c06f
[SW5] 김진규 <rlawlsrb16736@gmail.com>
2023-07-13 12:46:00 +0900

    feat: 관리자 주문페이지 주문상품 목록 생성

--- src/views/admin-order/admin-order.js
+++ src/views/admin-order/admin-order.js
@@ -1,15 +1,35 @@
+//서버에서 주문상품목록 데이터 가져옴
 const getOrderList = async () => {
   try{
     const response = await fetch('/api/orders');
     const data = await response.json();
-
     return data;
   }catch(e){
     console.error(e);
   }
 }
-window.addEventListener("load", async ()=>{
-
+//주문상품목록 템플릿
+const orderTemplate = (order) => {
+  return `
+          <td>
+              <input type="checkbox" class="selectCheck">
+          </td>
+          <td class="user-info">${order.userId.email}[${order.userId.name}]</td>
+          <td>${order.products[0].id} 외 ${order.products.length}건</td>
+          <td>${order.status}</td>`;
+}
+//주문상품목록 생성
+const createOrderList = async () => {
+  const adminTbl = document.querySelector("#adminTbl");
   const orderList = await getOrderList();
-  console.log(orderList);
+
+  orderList && orderList.map((order) => {
+      const tr = document.createElement("tr");
+      tr.innerHTML = orderTemplate(order);
+      adminTbl.append(tr);
+  })
+}
+
+window.addEventListener("load", async ()=>{
+  createOrderList();
 })
