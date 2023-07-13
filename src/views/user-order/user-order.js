0958f1df34ac4320c9f91c6e02c488b72f9ee7dd
[SW5] 김경연 <argand0304@gmail.com>
2023-07-13 18:54:50 +0900

    feat:사용자_주문조회_구현

--- src/views/user-order/user-order.js
+++ src/views/user-order/user-order.js
@@ -9,35 +9,40 @@ async function userOrder() {
     // 주문 정보 가져오기
     const response = await fetch("/api/orders/me");
     const orders = await response.json();
-
-    if (!orders.length) { // 주문 정보가 없을 때 innerHTML로 상품없을때의 문구 출력되도록 수정.
+    if (!orders.length) { 
       document.querySelector(".my-body").innerHTML = orderEmpty;
       return;
     }
 
     // 주문 정보가 있을 때
     let orderList = "";
+    
     for (let order of orders) {
       // 각 주문 내의 모든 제품 정보 표시
       let productInfo = "";
+      let totalPrice = order.totalAmount;
       for (let product of order.products) {
-        productInfo += `${product.productData.name} / ${product.quantity}개<br />`;
+        productInfo += `${product.id.title} / ${product.quantity}권<br />`;
+
       }
 
-      // 주문 정보를 HTML 표 형태로 추가. 순서대로 날짜, 상품정보, 배송상태
+      const orderTimestamp = parseInt(order._id.toString().substring(0, 8), 16) * 1000;
+      const orderDate = new Date(orderTimestamp).toLocaleDateString();
+      const orderTime = new Date(orderTimestamp).toLocaleTimeString();
       orderList += `<tr>
-        <td>${new Date(order._id.getTimestamp()).toLocaleDateString()}</td>
+        <td>${orderDate} ${orderTime}</td>
         <td>${productInfo}</td>
-        <td>${order.deliveryStatus}</td>
-        <td><button${order.deliveryStatus !== '상품 준비중' ? ' disabled' : ''}>주문취소</button></td>
+        <td>${totalPrice}원</td> <!-- 총 가격 표시 -->
+        <td>${order.deliveryStatus}</br><button data-id="${order._id}">주문취소</button></td>
       </tr>`;
     }
 
     const table = `<table>
       <thead>
-        <th>날짜</th>
-        <th>주문정보</th>
-        <th>주문상태</th>
+        <th>주문 날짜</th>
+        <th>주문 정보</th>
+        <th>총 가격</th>
+        <th>주문 상태</th>
         <th></th>
       </thead>
       <tbody>
@@ -48,6 +53,29 @@ async function userOrder() {
     // 주문 정보를 화면에 표시
     document.querySelector(".my-body").innerHTML = table;
 
+
+    document.querySelectorAll('button[data-id]').forEach(button => {
+      button.addEventListener('click', async () => {
+        const orderId = button.dataset.id;
+        try {
+          const response = await fetch(`/api/order/cancel/${orderId}`, {
+            method: 'DELETE',
+          });
+          if (response.ok) {
+            const data = await response.json();
+            // 주문 취소 후 페이지 새로고침
+            window.alert(data.message);
+            window.location.reload();
+          } else {
+            const data = await response.json();
+            window.alert(data.message);
+          }
+        } catch (err) {
+          const data = await response.json();
+          window.alert(data.message);
+        }
+      });
+    });
   } catch (err) {
     console.error("Error:", err);
     document.querySelector(".my-body").innerHTML = `<p>주문 정보를 불러오는 중 오류가 발생했습니다. 다시 시도해 주세요.</p>`;
