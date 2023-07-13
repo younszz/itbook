ffa101e68c018c7e7b6b3cc2411eb467f504932c
[SW5] 홍승진 <cmikal47@gmail.com>
2023-07-13 20:55:37 +0900

    fix: totalamount 오류

--- src/views/order/order.js
+++ src/views/order/order.js
@@ -159,7 +159,7 @@ document.querySelector(".payment-btn").addEventListener("click", async (e) => {
   const addressDetail = document.querySelector("#address2").value;
   const check1 = document.querySelector("#check-btn1").checked;
   const check2 = document.querySelector("#check-btn2").checked;
-  const tPrice = parseInt(document.querySelector(".total-price").innerHTML);
+  const tPrice = document.querySelector(".total-price").innerHTML;
   
   if (!address || !addressDetail) {
     alert("배송지를 입력해주세요.");
@@ -169,13 +169,12 @@ document.querySelector(".payment-btn").addEventListener("click", async (e) => {
     alert("필수 항목에 동의해 주세요.");
     return false;
   }
-  console.log(document.querySelector(".total-price").innerHTML);
   if (confirm("결제하시겠습니까?")) {
     const data = {
       address: address,
       addressDetail: addressDetail,
       products: JSON.parse(localStorage.getItem("selectedItems")),
-      totalAmount:  Number(tPrice.replace(/[^0-9]/g,"")),
+      totalAmount: Number(tPrice.replace(/[^0-9]/g,"")),
     };
 
     try {
